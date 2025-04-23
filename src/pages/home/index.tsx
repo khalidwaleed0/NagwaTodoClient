22dc3d680de1d79f7e20315d1770d6185d129d40
Khalid Waleed <khalidwaleed0@outlook.com>
2025-04-23 11:01:14 +0200

    Added card header with badge and greeting

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import { TasksContainer } from "./tasksContainer";
 import { ListsContainer } from "./ListsContainer";
 import "./style.css";
+import { StorageService } from "../../services/storage";
 
 export function Home() {
   const [isListOpen, setListOpen] = useState(false);
+  const username = StorageService.getUserName();
   return (
     <div className="home">
       <div className="home-card">
         <header>
-          <span className="badge"></span>
-          <span className="greet">Let's make today great!</span>
+          <span className="badge">{username?.charAt(0)}</span>
+          <span className="greet">Welcome back, {username}!</span>
         </header>
         <div className="home-content">{isListOpen ? <TasksContainer /> : <ListsContainer />}</div>
       </div>
